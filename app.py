76a0f887751cb7b93bb1d23ef505c5e714966880
zmdsn <zmdsn@126.com>
2024-12-13 16:36:07 +0800

    rss test

--- app.py
+++ app.py
@@ -2,20 +2,27 @@ from flask import Flask
 import requests
  
 app = Flask(__name__)
- 
- 
-@app.route('/')
-def index():
-    # 这里可以使用requests.request来实现各种http方法
-    req = requests.get('http://127.0.0.1:400')
-    # 响应体返回，这里是字符串
+
+
+@app.route('/rss')
+def index1():
+    req = requests.get(f'http://127.0.0.1:1200')
     return req.text
+
+@app.route('/rss/<path:name>')
+def index(name):
+
+    req = requests.get(f'http://127.0.0.1:1200/{name}')
+    return req.text
+
  
+@app.route('/test')
+def test():
+    return "Hello test."
  
-@app.route('/proxy')
-def proxy():
-    return "Hello Flask."
- 
+@app.route('/user/<username>')
+def show_user(username):
+    return 'User: %s' % username
  
 if __name__ == '__main__':
     app.run('0.0.0.0', port=80, debug=True)
